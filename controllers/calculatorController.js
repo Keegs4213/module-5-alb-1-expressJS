6fde01c948b8c975f5b2f813d57d34b73702fe19
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-04-06 00:42:43 +1200

    checked, completed, passed

--- controllers/calculatorController.js
+++ controllers/calculatorController.js
@@ -1,17 +1,28 @@
 // page 41 - module 5
 
-const addNumbers = (req, res) => {
+const addGetNumbers = (req, res) => {
 
-    let number1=parseInt(req.query.number1);
-    let number2=parseInt(req.query.number2);
-    let sum=number1+number2
-
-    
+    let number1=parseInt(req.body.number1);
+    let number2=parseInt(req.body.number2);
+    let sum= number1 + number2
 
     res.status(200)
     res.json({result:sum})
 }
 
+const addPostNumbers = (req, res) => {
+  let number1 = parseInt(req.body.number1);
+  let number2 = parseInt(req.body.number2);
+  let sum = number1 + number2;
+
+  res.status(200);
+  res.json({ result: sum });
+};
+
+
+
+
 module.exports = {
-    addNumbers
-}
+  addGetNumbers,
+  addPostNumbers
+};
--- routes/calculatorRoute.js
+++ routes/calculatorRoute.js
@@ -4,6 +4,42 @@ var router = express.Router()
 
 var calculatorController = require('../controllers/calculatorController')
 
-router.get('/add', calculatorController.addNumbers)
+//get request returns the form,
+router.get("/add", calculatorController.addGetNumbers);
+
+//post request returns the result
+router.post("/add", calculatorController.addPostNumbers);
+
+
+// path params
+router.get('/subtract/:number1/:number2', (req, res) => {
+    let number1 = parseInt(req.params.number1)
+    let number2 = parseInt(req.params.number2)
+    let result = number1 - number2
+    console.log(req.params)
+    res.status(200)
+    res.json({result:result})
+})
+
+// request body - change to post method
+router.post('/multiply', (req, res) => {
+    let number1 = parseInt(req.body.number1)
+    let number2 = parseInt(req.body.number2)
+    let result = number1 * number2
+    console.log(result)
+    res.status(200)
+    res.json({result:result})
+})
+
+// query parameters
+router.get('/divide', (req, res) => {
+    let number1 = parseInt(req.query.number1)
+    let number2 = parseInt(req.query.number2)
+    let result = number1 / number2
+    console.log(result)
+    res.status(200)
+    res.json({result:result})
+})
+
 
 module.exports = router
